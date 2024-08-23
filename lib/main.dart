662c04f2a5317d757c57ccee0af66b7a7e5aaac7
mahmo <mahmoud.a@hybridsolutions.com>
2024-08-23 23:57:09 +0300

    Add New features to app and fix some issues and enhancement code

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,15 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:yemenshabab/app.dart';
 import 'package:yemenshabab/bloc_observer.dart';
 import 'package:yemenshabab/core/config/config.dart';
 import 'package:yemenshabab/core/helper/cache_helper.dart';
 import 'package:yemenshabab/core/utils/firebase_options.dart';
+
+const String appId = '0d941047-04fd-45e5-86b8-37f4bf5e9d0b';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +23,10 @@ Future<void> main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  await OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize(appId);
+  OneSignal.Notifications.requestPermission(true);
 
   await JustAudioBackground.init(
     androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
