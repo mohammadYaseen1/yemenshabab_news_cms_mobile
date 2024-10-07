9e00c33da34f13e04d3fdf00ba548035ea110737
mahmoudalkhdour <mahmoud.h.alkhdour@gmail.com>
2024-10-08 00:48:34 +0300

    Shorebird IOS

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:yemenshabab/app.dart';
 import 'package:yemenshabab/bloc_observer.dart';
@@ -12,6 +13,8 @@ import 'package:yemenshabab/shared/config/config.dart';
 import 'package:yemenshabab/shared/constants/constants.dart';
 import 'package:yemenshabab/shared/local/cache_helper.dart';
 
+const String appId = '0d941047-04fd-45e5-86b8-37f4bf5e9d0b';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CacheHelper.init();
@@ -19,11 +22,17 @@ Future<void> main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  await OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize(appId);
+  OneSignal.Notifications.requestPermission(true);
+
   await JustAudioBackground.init(
     androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
     androidNotificationChannelName: 'Audio playback',
     androidNotificationOngoing: true,
   );
+
   tz.initializeTimeZones();
   Config.timeZoneName = await FlutterTimezone.getLocalTimezone();
   Bloc.observer = MyBlocObserver();
